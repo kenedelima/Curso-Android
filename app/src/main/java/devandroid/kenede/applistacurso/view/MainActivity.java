1226c2f86070b8b0edd15787c9e96df141886a25
kenede de lima pires <55933535+kenedelima@users.noreply.github.com>
2023-04-08 07:27:41 -0300

    Layout Correto App Lista Vip com id TextView EditText

--- app/src/main/java/devandroid/kenede/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/kenede/applistacurso/view/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Declarando um objeto pessoa
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
 
     @Override
@@ -19,7 +23,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // crinado um objeto pessoa (istanciando)
+        // Conforme o seu Modelo (Template)
         pessoa = new Pessoa();
+        //Atribuindo conteudo para o objeto
+        pessoa.setPrimeiroNome("Marco");
+        pessoa.setSobreNome("Maddo");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("81-99225645");
 
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobreNome("Alves");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("11-99357899");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada = 0;
     }
 }
